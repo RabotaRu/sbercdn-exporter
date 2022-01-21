647b9ec3185fdf35119538414fcc99d9fe644e63
Олег Марьин <o.marin@rabota.ru>
2022-01-21 10:52:29 +0300

    first working version

---
+++ common/common.go
@@ -0,0 +1,32 @@
+package common
+
+import (
+	"time"
+)
+
+type Auth struct {
+	Id            string        `yaml:"id"`
+	Username      string        `yaml:"username"`
+	Password      string        `yaml:"password"`
+	Urn           string        `yaml:"urn"`
+	TokenLifetime time.Duration `yaml:"token_lifetime"`
+}
+
+type ClientConf struct {
+	Url            string            `yaml:"url"`
+	Endpoints      map[string]string `yaml:"endpoints"`
+	Auth           Auth              `yaml:"auth"`
+	MaxQueryTime   time.Duration     `yaml:"max_query_time"`
+	ScrapeInterval time.Duration     `yaml:"scrape_interval"`
+}
+
+type ListenConf struct {
+	Address     string `yaml:"address"`
+	CertFile    string `yaml:"cert_file"`
+	PrivkeyFile string `yaml:"privkey_file"`
+}
+
+type AppConf struct {
+	Listen ListenConf `yaml:"listen"`
+	Client ClientConf `yaml:"api"`
+}
